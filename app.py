544de3035c5f2e12b9ecaaf2844ae937716417ff
mongoosepetter38 <michaelgaryscott141@gmail.com>
2026-06-14 14:10:40 -0400

    fix port for deployment

--- app.py
+++ app.py
@@ -44,4 +44,6 @@ def delete(index):
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=False)
